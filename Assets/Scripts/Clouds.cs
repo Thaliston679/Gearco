1bbcda28fed9257bed95dd456e3a433a7a2a9df4
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-05-19 23:44:30 -0300

    Script nuvens

--- Assets/Scripts/Clouds.cs
+++ Assets/Scripts/Clouds.cs
@@ -16,10 +16,12 @@ public class Clouds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + velCloud, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x - velCloud, transform.position.y, transform.position.z);
+        //transform.position = new Vector3(transform.position.x - (cam.position.x * velCloud), transform.position.y, transform.position.z);
 
-        if(transform.position.x < cam.position.x - distCloud)
+        if (transform.position.x < cam.position.x - distCloud)
         {
+            //float posY = Random.RandomRange(-20, -14);
             transform.position = new Vector3(cam.position.x + distCloud, transform.position.y, transform.position.z);
         }
     }

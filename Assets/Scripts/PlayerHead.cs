a1e51b02653c0336b959a87638e95c1fd6b579f0
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-05-24 17:22:12 -0300

    Afogamento funcionando perfeitamente

---
+++ Assets/Scripts/PlayerHead.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHead : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Quicksand") //Afundar a cabeça na areia movediça
+        {
+            player.GetComponent<Move2D>().playerHP = 0;
+        }
+    }
+}

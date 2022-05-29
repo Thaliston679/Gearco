bd32c00a160df9bf3c9f44cea52718cf271cbdaf
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-05-29 13:32:56 -0300

    Update CactusAnimationControl.cs

--- Assets/CactusAnimationControl.cs
+++ Assets/CactusAnimationControl.cs
@@ -11,6 +11,67 @@ public class CactusAnimationControl : StateMachineBehaviour
 
         if (cacto.GetAggressive())
         {
+           if (animator.GetBool("ToAggressive"))
+           {
+                animator.SetBool("ToAggressive", false);
+                animator.SetBool("Aggressive", true);
+           }
+        }
+        else
+        {
+            if (animator.GetBool("ToPeaceful"))
+            {
+                animator.SetBool("ToPeaceful", false);
+                animator.SetBool("Peaceful", true);
+            }
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Cacto cacto = animator.gameObject.GetComponent<Cacto>();
+
+        if (cacto.GetAggressive())
+        {
+            if (animator.GetBool("Peaceful"))
+            {
+                animator.SetBool("Peaceful", false);
+                animator.SetBool("ToAggressive", true);
+            }
+        }
+        else
+        {
+            if (animator.GetBool("Aggressive"))
+            {
+                animator.SetBool("Aggressive", false);
+                animator.SetBool("ToPeaceful", true);
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+
+    /*
+     *
+    if (cacto.GetAggressive())
+        {
             if (animator.GetBool("Peaceful"))
             {
                 animator.SetBool("ToAggressive", true);
@@ -31,30 +92,6 @@ public class CactusAnimationControl : StateMachineBehaviour
                 animator.SetBool("Peaceful", true);
             }
         }
-
-    }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+     *
+     */
 }

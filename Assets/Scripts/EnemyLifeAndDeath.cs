ee104071cda78ec30eb9ffb646bea9a75df5d25f
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-05-29 00:29:41 -0300

    EnemyLifeAndDeath
    Script EnemyLifeAndDeath criado para controlar o valor da vida dos inimigos e instanciar a explosão de morte do mesmo

---
+++ Assets/Scripts/EnemyLifeAndDeath.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLifeAndDeath : MonoBehaviour
+{
+    public int enemyHP;
+    public GameObject smokeExplosion;
+
+    void Update()
+    {
+        if(enemyHP <= 0)
+        {
+            GameObject explosion = Instantiate(smokeExplosion, this.transform.position, Quaternion.identity);
+            Destroy(this);
+        }
+    }
+
+    public void SetEnemyHp(int i)
+    {
+        enemyHP = i;
+    }
+
+    public int GetEnemyHp()
+    {
+        return enemyHP;
+    }
+}

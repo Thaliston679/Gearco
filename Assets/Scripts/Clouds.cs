7a5e2a2d2f20376446d5e171ffa473b06990240d
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-06-09 09:22:03 -0300

    Respawn de nuvens após cair na areia ou do cenário

--- Assets/Scripts/Clouds.cs
+++ Assets/Scripts/Clouds.cs
@@ -13,10 +13,11 @@ public class Clouds : MonoBehaviour
         transform.position = new Vector3(transform.position.x - velCloud, transform.position.y, transform.position.z);
         //transform.position = new Vector3(transform.position.x - (cam.position.x * velCloud), transform.position.y, transform.position.z);
 
-        if (transform.position.x < cam.position.x - distCloud)
+        if (transform.position.x < cam.position.x - distCloud || transform.position.x > cam.position.x + distCloud*2)
         {
             //float posY = Random.RandomRange(-20, -14);
-            transform.position = new Vector3(cam.position.x + distCloud, transform.position.y, transform.position.z);
+            float distR = Random.Range(distCloud, distCloud * 2);
+            transform.position = new Vector3(cam.position.x + distR, transform.position.y, transform.position.z);
         }
     }
 }

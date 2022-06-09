11b66b2100428234a71ea29d7a6ffba316e297b6
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-06-09 13:50:30 -0300

    Novo parallax funcionando perfeitamente
    Aplicado parallax nas nuvens e nas montanhas. Câmera se mantém suave, sem ticks nas montanhas e as nuvens foram configuradas corretamente.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject target;
-    public float smoothSpeed = 0.125f;
-    public float distX = 1;
+    public float smoothSpeed = 2.5f; // 0.125f sem Time.deltaTime, 2.5f com
+    public float distX = 1.5f;
 
     public Vector3 offset;
 
@@ -15,7 +15,7 @@ public class CameraFollow : MonoBehaviour
         bool directionR = target.GetComponent<Move2D>().GetCameraViewingSideRight();
         offset.x = directionR ? distX : -distX;
         Vector3 desiredPosition = target.transform.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
 
--- Assets/Scripts/Clouds.cs
+++ Assets/Scripts/Clouds.cs
@@ -13,10 +13,10 @@ public class Clouds : MonoBehaviour
         transform.position = new Vector3(transform.position.x - velCloud, transform.position.y, transform.position.z);
         //transform.position = new Vector3(transform.position.x - (cam.position.x * velCloud), transform.position.y, transform.position.z);
 
-        if (transform.position.x < cam.position.x - distCloud || transform.position.x > cam.position.x + distCloud*2)
+        if (transform.position.x < cam.position.x - distCloud*2 || transform.position.x > cam.position.x + distCloud*4)
         {
             //float posY = Random.RandomRange(-20, -14);
-            float distR = Random.Range(distCloud, distCloud * 2);
+            float distR = Random.Range(distCloud*2, distCloud * 4);
             transform.position = new Vector3(cam.position.x + distR, transform.position.y, transform.position.z);
         }
     }

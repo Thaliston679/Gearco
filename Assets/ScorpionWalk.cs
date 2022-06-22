3855c4f9a9a67e577c7e0ae92c01940779f483b3
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-06-22 00:01:56 -0300

    Boss

---
+++ Assets/ScorpionWalk.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorpionWalk : StateMachineBehaviour
+{
+    public float speed = 3f;
+    public float impactAtkRange = 5f;
+    Transform player;
+    Rigidbody2D rb;
+    ScorpionBoss boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss = animator.GetComponent<ScorpionBoss>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.FlipX();
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.deltaTime);
+        rb.MovePosition(newPos);
+
+        if (Vector2.Distance(player.position, rb.position) <= impactAtkRange)
+        {
+            animator.SetTrigger("ImpactAtk");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("ImpactAtk");
+    }
+}
--- Assets/Scripts/ScorpionBoss.cs
+++ Assets/Scripts/ScorpionBoss.cs
@@ -9,12 +9,48 @@ public class ScorpionBoss : MonoBehaviour
 
     public GameObject target;
     public GameObject poisonBullet;
+    public GameObject impactEffect;
 
     public GameObject forRight;
     public GameObject forLeft;
+    private SpriteRenderer img;
+    private bool flipX = false; 
 
-    void MeleeAtk()
+    private void Start()
     {
+        img = GetComponent<SpriteRenderer>();
+    }
+    public void FlipX()
+    {
+        if (transform.position.x > target.transform.position.x && flipX)
+        {
+            flipX = false;
+            img.flipX = flipX;
+        }
+        else if (transform.position.x < target.transform.position.x && !flipX)
+        {
+            flipX = true;
+            img.flipX = flipX;
+        }
+    }
+
+    public void MeleeAtk()
+    {
+        if (!flipX)
+        {
+            Vector3 impactPos = new Vector3(transform.position.x - 3f, transform.position.y, transform.position.z);
+            GameObject impact = Instantiate(impactEffect, impactPos, Quaternion.identity);
+            SpriteRenderer spriteRenderer = impact.GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = false;
+        }
+        else if (flipX)
+        {
+            Vector3 impactPos = new Vector3(transform.position.x + 3f, transform.position.y, transform.position.z);
+            GameObject impact = Instantiate(impactEffect, impactPos, Quaternion.identity);
+            SpriteRenderer spriteRenderer = impact.GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = true;
+        }
+
 
     }
 
